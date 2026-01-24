f7f0bf7c2fd7d040dc790b641becb2a3aa3d4609
Shruti270903 <shrutikashyap2709@gmail.com>
2026-01-24 13:35:14 +0530

    Complete authentication system and ignore env file

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,14 +1,66 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-export const register = async(requestAnimationFrame, res)=>{
+import userModel from '../models/userModel.js';
+export const register = async(req, res)=>{
     const {name, email, password} = req.body;
-
     if(!name || !email || !password){
         return res.status(400).json({success:false, message:"Details missing, All fields are required"});
     }
     try{
+        const existingUser = await userModel.findOne({email});
+        if(existingUser){
+            return res.status(400).json({success:false, message:"User already exists"});
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new userModel({name, email, password:hashedPassword});
+        await user.save();
+        const token = jwt.sign({id: user._id},process.env.JWT_SECRET, {expiresIn: '7d'});
+
+        res.cookie('token', token, {httpOnly: true,secure:process.env.NODE_ENV === 'production',
+            sameSite:process.env.NODE_ENV ==='production'?'none' :'strict',
+             maxAge: 7*24*60*60*1000});
         res.status(200).json({success:true, message:"User registered successfully"});
     }catch(error){
         res.status(500).json({success:false, message:"Error in registering user"});
+    }
+}
+
+export const login = async(req, res)=>{
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.status(400).json({success:false, message:"Details missing, All fields are required"});
+
+    }
+    try{
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.status(400).json({success:false, message:"User does not exist"});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({success:false, message:"Invalid credentials"});
+        }
+        const token = jwt.sign({id: user._id},process.env.JWT_SECRET, {expiresIn: '7d'});
+        res
+        .cookie('token', token, {httpOnly: true,secure:process.env.NODE_ENV === 'production',
+            sameSite:process.env.NODE_ENV ==='production'?'none' :'strict',
+                maxAge: 7*24*60*60*1000})
+        res.status(200)
+        .json({success:true, message:"User logged in successfully"});
+    }catch(error){
+        res.status(500).json({success:false, message:"Error in logging in"});
+    }
+}
+export const logout = async(req,res)=>{
+    try{
+        res.clearCookie('token', {
+        httpOnly:true,
+        secure
+        :process.env.NODE_ENV === 'production',
+        sameSite:process.env.NODE_ENV === 'production'? 'none': 'strict',
+        })
+        return res.status(200).json({success:true, message: 'Logged Out'});
+    }catch(error){
+        return res.status(500).json({success:false, message: error.message});
     }
 }
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
-
+import authRouter from './routes/authRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
@@ -12,7 +12,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
+//API EndPoints
 app.get('/', (req, res)=>res.send("API is running fine"));
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
