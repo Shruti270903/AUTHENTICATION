5838c2b525ff53821ddee11e7d945a1a0ec15ec6
Shruti270903 <shrutikashyap2709@gmail.com>
2026-02-03 16:54:59 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ connectDB();
 const allowedOrigins = ['http://localhost:5173'];
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:allowedOrigins, credentials: true}));
+app.use(cors({
+  origin: "https://authentication-frontend-dsav.onrender.com",
+  credentials: true
+}));
 
 //API EndPoints
 app.get('/', (req, res)=>res.send("API is running fine"));
