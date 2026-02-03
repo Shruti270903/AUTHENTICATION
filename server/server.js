5e6cfc7418cf032ae8bacba50a9265c42a078229
Shruti270903 <shrutikashyap2709@gmail.com>
2026-02-03 17:16:35 +0530

    commit allowedOrigin changes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['https://authentication-frontend-dsav.onrender.com'];
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
